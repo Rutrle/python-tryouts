02bb1714c09f8d21405aaacf0773683c32f522f5
Jakub <rutrle@seznam.cz>
2020-11-08 12:26:21 +0100

    tryout of pandas dataframe

---
+++ pandas_dataframe_tryout.py
@@ -0,0 +1,13 @@
+
+
+import pandas as pd
+'''
+x = pd.DataFrame({'name': ['George', 'Elisabeth'], 'age': [
+    50, 30], 'nationality': ['UK', 'USA']})
+'''
+
+
+df = pd.DataFrame({"Name": ["Braund, Mr. Owen Harris", "Allen, Mr. William Henry", "Bonnell, Miss. Elizabeth"], "Age": [22, 35, 58], "Sex": ["male", "male", "female"]}
+                  )
+
+print(df)
