83666e55e7efaa0c7c0c2b427f8d65289f870911
Jakub <rutrle@seznam.cz>
2020-11-19 23:52:28 +0100

    printing object type, differences between list and tuple

---
+++ finding_out_type.py
@@ -0,0 +1,29 @@
+
+
+my_var = 'foobar'
+
+
+print(type(my_var))
+
+
+my_list = [123, 'foobar']
+
+print(type(my_list))
+print(type(my_list[0]))
+print(type(my_list[1]))
+
+
+my_list[0] = 456
+
+print(my_list)
+
+my_tuple = (123, 'foobar')
+
+print(type(my_tuple))
+
+'''
+my_tuple[0] = 456
+
+my_tuple.append(987)
+'''
+print(type(my_tuple))
